a6ca6f161151752db6653c7fa6083b24548b629d
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2022-06-04 13:59:54 -0400

    env procfile stuff

--- app.js
+++ app.js
@@ -113,6 +113,6 @@ app.post("/delete", (req, res) => {
   })
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT, function() {
+  console.log("Server listening...");
 });
